180e7274d653e5adf0c032ea8ce42a7b799fdc2b
Matthias Röll <matthias.roell@mrll.de>
2015-05-11 12:46:11 +0200

    Dokumentation zu LCParser hinzugefügt

--- src/Model/LCParser.cpp
+++ src/Model/LCParser.cpp
@@ -5,6 +5,7 @@
 #include "LCParser.h"
 
 #pragma mark RegEx
+/* ---------------------------------------------------------------------------------------------------------------- */
 
 const QRegExp * LCParser::moveExp      = new QRegExp("^\\s?(MOVE)\\s+(\\d){1,4}\\s*,(\\s)*(\\d){1,4}(?:\\s*\\#+.*|\\s*)$",
                                                      Qt::CaseSensitive,
@@ -17,6 +18,11 @@ const QRegExp * LCParser::commentExp   = new QRegExp("\\s?\\#+.*$",
                                                      QRegExp::RegExp);
 
 #pragma mark LCParserError
+/* ---------------------------------------------------------------------------------------------------------------- */
+
+/* ==================== */
+/* Konstruktoren        */
+/* ==================== */
 
 LCParserError::LCParserError(QString string, int line, LCErrorCode code) {
     this->string = string;
@@ -30,41 +36,59 @@ LCParserError::LCParserError() {
 }
 
 #pragma mark LCParserCommand
+/* ---------------------------------------------------------------------------------------------------------------- */
 
-LCParserCommand::LCParserCommand(LCCommandCode command, int parameter1, int parameter2) {
-    this->command = command;
-    this->parameter1 = parameter1;
-    this->parameter2 = parameter2;
+/* ==================== */
+/* Konstruktoren        */
+/* ==================== */
+
+LCParserCommand::LCParserCommand(LCCommandCode command, int * parameter, int parameterCount) {
+    this->command           = command;
+    this->parameter         = parameter;
+    this->parameterCount    = parameterCount;
 }
 LCParserCommand::LCParserCommand() {
-    this->command = LCUnknownCommand;
-    this->parameter1 = 0;
-    this->parameter2 = 0;
+    this->command           = LCUnknownCommand;
+    this->parameter         = (int *) malloc(sizeof(int) * 2);
+    this->parameterCount    = 2;
 }
 
 #pragma mark LCParser
+/* ---------------------------------------------------------------------------------------------------------------- */
+
+/* ==================== */
+/* Konstruktor          */
+/* ==================== */
 
 LCParser::LCParser() {
     this->validators = new QVector<validatorFunction>();
 
     this->validators->append(LCParser::moveExpValidator);
     this->validators->append(LCParser::powerExpValidator);
+
+    // Weitere Validatoren können in vererbten Klassen hinzugefügt werden.
 }
+
+/* ==================== */
+/* parse                */
+/* ==================== */
 
 bool LCParser::parse(QString code, QVector<LCParserCommand> * commands, QVector<LCParserError> * errors) {
     if (commands == NULL || errors == NULL) {
         return false;
     }
 
-    QStringList codeLines = code.split("\n", QString::SplitBehavior::KeepEmptyParts);   // Generate line list
+    // Der Code wird in Zeilen aufgespalten und iteriert
+    QStringList codeLines = code.split("\n", QString::SplitBehavior::KeepEmptyParts);
     QStringList::iterator it = codeLines.begin();
 
     while (it != codeLines.end()) {
-        if (it->isEmpty() || commentExp->exactMatch(it->toUpper())) {          // Check if empty or comment
-            // Do nothing
+        if (it->isEmpty() || commentExp->exactMatch(it->toUpper())) {
+            // Bei leeren Zeilen oder Kommentaren ist nichts zu tun
         } else {
             LCParserCommand command;
 
+            // Validatoren testen und gefundene Befehle speichern
             for (int i = 0; i < validators->count(); ++i) {
                 if (validators->at(i)(*it, &command)) {
                     commands->append(command);
@@ -72,6 +96,8 @@ bool LCParser::parse(QString code, QVector<LCParserCommand> * commands, QVector<
                 }
             }
 
+            // Kein Befehl gefunden wenn command noch LCUnknownCommand ist
+            // Dann nach Fehlertyp suchen und speichern
             if (command.command == LCUnknownCommand) {
                 errors->append(findLineError(*it, it - codeLines.begin()));
             }
@@ -80,34 +106,54 @@ bool LCParser::parse(QString code, QVector<LCParserCommand> * commands, QVector<
         ++it;
     }
 
-    return errors->isEmpty();   // No errors if isEmpty
+    // Keine Fehler wenn errors leer ist
+    return errors->isEmpty();
 }
 
+/* ==================== */
+/* findLineError        */
+/* ==================== */
+
 LCParserError LCParser::findLineError(QString string, int line) {
-    // TODO: Implement error detection
+    // TODO: Fehlererkennung implementieren
     return LCParserError(string, line, LCErrNone);
 }
 
+/* ==================== */
+/* moveExpValidator     */
+/* ==================== */
+
 bool LCParser::moveExpValidator(QString string, LCParserCommand * command) {
-    if (command != NULL && LCParser::moveExp->exactMatch(string.toUpper())) {
-        QStringList list = LCParser::moveExp->capturedTexts();
-        // List contains [0] full string, [1] MOVE, [2] parameter 1, [3] parameter 2
-        command->command = LCMoveCommand;
-        command->parameter1 = list[2].toInt();
-        command->parameter2 = list[3].toInt();
-        return true;
+    if (command != NULL && command->parameterCount >= 2) {
+        if (LCParser::moveExp->exactMatch(string.toUpper())) {
+            QStringList list = LCParser::moveExp->capturedTexts();
+
+            // Liste enthält [0] ganzen string, [1] MOVE, [2] parameter 1, [3] parameter 2
+            command->command        = LCMoveCommand;
+            command->parameter[0]   = list[2].toInt();
+            command->parameter[1]   = list[3].toInt();
+
+            return true;
+        }
     }
     return false;
 }
 
+/* ==================== */
+/* powerExpValidator    */
+/* ==================== */
+
 bool LCParser::powerExpValidator(QString string, LCParserCommand * command) {
-    if (command != NULL && LCParser::powerExp->exactMatch(string.toUpper())) {
-        QStringList list = LCParser::powerExp->capturedTexts();
-        // List contains [0] full string, [1] LASER, [2] parameter 1
-        command->command = LCPowerCommand;
-        command->parameter1 = list[2].toInt();
-        command->parameter2 = 0;
-        return true;
+    if (command != NULL && command->parameterCount >= 1) {
+        if (LCParser::powerExp->exactMatch(string.toUpper())) {
+            QStringList list = LCParser::powerExp->capturedTexts();
+
+            // Liste enthält [0] ganzen string, [1] LASER, [2] parameter 1
+            command->command        = LCPowerCommand;
+            command->parameter[0]   = list[2].toInt();
+
+            return true;
+        }
     }
     return false;
 }
--- src/Model/LCParser.h
+++ src/Model/LCParser.h
@@ -14,110 +14,171 @@
 //#include "LCState.h"
 
 #pragma mark Enums
+/* ---------------------------------------------------------------------------------------------------------------- */
 
+/**
+ * Fehlertypenenumaration
+ */
 typedef enum {
-    LCErrNone = 0,              // No Error (on init)
-    LCErrSyntax = 1,            // Syntaxerror
-    LCErrParamaterType = 2,     // Wrong parameter type
-    LCErrParameterCount = 3     // Wrong parameter count
+    LCErrNone = 0,              /**< Kein Fehler (Platzhalter bei Initialisierung)  */
+    LCErrSyntax = 1,            /**< Syntaxfehler                                   */
+    LCErrParamaterType = 2,     /**< Falscher Parametertyp                          */
+    LCErrParameterCount = 3     /**< Falsche Parameteranzahl                        */
 } LCErrorCode;
 
+/**
+ * Befehlsenumeration
+ */
 typedef enum {
-    LCUnknownCommand = 0,       // Unknown command (on init)
-    LCMoveCommand = 1,          // Move command (MOVE 100, 200)
-    LCPowerCommand = 2          // Power command (LASER ON)
+    LCUnknownCommand = 0,       /**< Unbekannter Befehl (Platzhalter bei Initialisierung/Validierung)   */
+    LCMoveCommand = 1,          /**< Move Befehl  (MOVE 100, 200)                                       */
+    LCPowerCommand = 2          /**< Power Befehl (LASER ON)                                            */
 } LCCommandCode;
 
 #pragma mark LCParserError
+/* ---------------------------------------------------------------------------------------------------------------- */
 
+/**
+ * Klasse zum Speichern von Fehlertypen und Daten
+ */
 class LCParserError {
 
 public:
     LCParserError(QString string, int line, LCErrorCode code);
     LCParserError();
 
-    QString string;         // Errorstring
-    int line;               // Line number
-    LCErrorCode code;       // Error code
+    QString string;         /**< Fehlerstring   */
+    int line;               /**< Zeile          */
+    LCErrorCode code;       /**< Fehlercode     */
 };
 
 #pragma mark LCParserCommand
+/* ---------------------------------------------------------------------------------------------------------------- */
 
+/**
+ * Klasse zum Speichern von Behlen und Parametern
+ */
 class LCParserCommand {
 
 public:
-    LCParserCommand(LCCommandCode command, int parameter1, int parameter2);
+    LCParserCommand(LCCommandCode command, int * parameter, int parameterCount);
     LCParserCommand();
 
-    LCCommandCode command;  // Command
-    int parameter1;         // Parameter 1
-    int parameter2;         // Parameter 2 (Only LCMoveCommand)
+    LCCommandCode     command;              /**< Befehl             */
+    int             * parameter;            /**< Parameter          */
+    int               parameterCount;       /**< Parameterannzahl   */
 };
 
 #pragma mark Typedefs
+/* ---------------------------------------------------------------------------------------------------------------- */
 
+/**
+ * Typendefinition für Validierungsfunktionen
+ *
+ * @param QString String zum Prüfen
+ * @param LCParserCommand* Zeiger auf einen Command zum speichern der gefundenen Werte
+ *
+ * @see LCParser::moveExpValidator LCParser::powerExpValidator
+ *
+ * @return Erfolgreiche Validierung
+ */
 typedef bool(*validatorFunction)(QString, LCParserCommand *);
 
 #pragma mark LCParser
+/* ---------------------------------------------------------------------------------------------------------------- */
 
+/**
+ * Parserklasse
+ */
 class LCParser {
 
 public:
+    /**
+     * Standartkonstruktor
+     *
+     * Der Konstruktor initialisiert den 'validators' Vector.
+     * Dieser kann in erbenden Klassen um neue Befehle erweitert werden.
+     */
     LCParser();
 
+    /**
+     * Parserfunktion
+     *
+     * Prüft den übergebenen Code zeilenweise mit allen Validierungsfunktionen in 'validators'.
+     * Dabei werden die Befehle in 'commands' und etwaige Fehler in 'errors' gespeichert.
+     *
+     * Gibt false zurück wenn 'commands' oder 'errors' gleich NULL sind.
+     *
+     * @param code Der zu parsende Codestring
+     * @param commands QVector zum Speichern gefundener Befehle
+     * @param errors QVector zum Speichern gefundener Fehler
+     *
+     * @return true wenn keine Fehler auftauchten.
+     */
     bool parse(QString code, QVector<LCParserCommand> * commands, QVector<LCParserError> * errors);
+
+    /**
+     * Fehleranalyse
+     *
+     * Sucht nach der Fehlerursache in einer Zeile. Und gibt einen LCParserError zurück.
+     *
+     * Die Fehler werden in folgender Reihenfolge gesucht:
+     * * Parametertyp
+     * * Parameteranzahl
+     * * Syntax
+     *
+     * @param string Zeilenstring
+     * @param line Zeilennummer
+     *
+     * @return Fehlerobjekt
+     */
     LCParserError findLineError(QString string, int line);
 
-#pragma mark RegEx Declaration
-
-    static const QRegExp * moveExp;
-    static const QRegExp * powerExp;
-    static const QRegExp * commentExp;
+    static const QRegExp * moveExp;             /**< MOVE Befehl Ausdruck               */
+    static const QRegExp * powerExp;            /**< LASER Befehl Ausdruck              */
+    static const QRegExp * commentExp;          /**< Kommentar Ausdruck                 */
 
 protected:
-#pragma mark RegEx Function Table
-
-    QVector<validatorFunction> * validators;
+    QVector<validatorFunction> * validators;    /**< Feld mit Validierungsfunktionen    */
 
 private:
-#pragma mark RegEx Functions
-
     static bool moveExpValidator(QString string, LCParserCommand * command);
     static bool powerExpValidator(QString string, LCParserCommand * command);
 };
 
 #pragma mark RegEx Descriptions
+/* ---------------------------------------------------------------------------------------------------------------- */
 
 /*
  * MOVE Expression - ^\s?(MOVE)\s+(\d){1,4}\s*,(\s)*(\d){1,4}(?:\s*\#+.*|\s*)$
  *
- * ^\s?                     - Begin of line with optional whitespaces
- * (MOVE)                   - Group 1: Word MOVE
- * \s+                      - One whitespace at minimum
- * (\d){1,4}                - Group 2: One to four digits
- * \s*,\s*                  - Comma seperated with optional whitespaces in between
- * (\d){1,4}                - Group 3: One to four digits
- * (?:\s*\#+.*|\s*)         - Either a comment (optional whitespaces + # + something) or just optional whitespaces
- * $                        - End of line
+ * ^\s?                     - Anfang der Zeile mit optionalen Leerzeichen
+ * (MOVE)                   - Gruppe 1: MOVE
+ * \s+                      - Mindstens 1 Leerzeichen
+ * (\d){1,4}                - Gruppe 2: Zahl zwischen 0 und 9999
+ * \s*,\s*                  - Trennung durch Komma mit optionalen Leerzeichen
+ * (\d){1,4}                - Gruppe 3: Zahl zwischen 0 und 9999
+ * (?:\s*\#+.*|\s*)         - Entweder ein Kommentarstring (# + Irgendwas) oder Leerzeichen (optional)
+ * $                        - Ende der Zeile
  */
 
 /*
  * LASER Expression - ^\s?(LASER)\s+(ON|OFF)(?:\s*\#+.*|\s*)$
  *
- * ^\s?                     - Begin of line with optional whitespaces
- * (LASER)                  - Group 1: Word LASER
- * \s+                      - One whitespace at minimum
- * (ON|OFF)                 - Group 2: Word ON or OFF
- * (?:\s*\#+.*|\s*)         - Either a comment (optional whitespaces + # + something) or just optional whitespaces
- * $                        - End of line
+ * ^\s?                     - Anfang der Zeile mit optionalen Leerzeichen
+ * (LASER)                  - Gruppe 1: LASER
+ * \s+                      - Mindstens 1 Leerzeichen
+ * (ON|OFF)                 - Gruppe 2: ON oder OFF
+ * (?:\s*\#+.*|\s*)         - Entweder ein Kommentarstring (# + Irgendwas) oder Leerzeichen (optional)
+ * $                        - Ende der Zeile
  */
 
 /*
  * COMMENT Expression - \s?\#+.*$
  *
- * ^\s?                     - Begin of line with optional whitespaces
- * \#+.*                    - # + something
- * $                        - End of line
+ * ^\s?                     - Anfang der Zeile mit optionalen Leerzeichen
+ * \#+.*                    - Kommentarstring (# + Irgendwas)
+ * $                        - Ende der Zeile
  */
 
 /* RegEx Tester: http://www.regexr.com */
